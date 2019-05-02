a982e9ece19a211efdaff255068d478d32c4379d
unknown <ui-jeong>
2019-05-03 07:05:30 +0900

    make Camera

--- osbprjt/src/pages/camera/camera.module.ts
+++ osbprjt/src/pages/camera/camera.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
+import { NgModule, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { IonicPageModule, ModalController } from 'ionic-angular';
 import { CameraPage } from './camera';
+import { componentFactoryName } from '@angular/compiler';
 
+@Component({
+  selector: 'app-ar-camera',
+  templateUrl: './camera.html',
+  styleUrls: ['./CameraPage.scss'],
+  encapsulation: ViewEncapsulation.None
+})
 @NgModule({
   declarations: [
     CameraPage,
@@ -10,4 +17,12 @@ import { CameraPage } from './camera';
     IonicPageModule.forChild(CameraPage),
   ],
 })
-export class CameraPageModule {}
+export class ArCameraPage implements OnInit{
+  constructor(private modalCtrl: ModalController){}
+
+  ngOnInit(){
+
+  }
+
+
+}
