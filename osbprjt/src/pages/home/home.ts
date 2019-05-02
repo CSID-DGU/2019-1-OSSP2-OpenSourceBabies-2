4f2c39a9831a6aa7d47b55b37778cfc96d8f145c
unknown <ui-jeong>
2019-05-03 06:42:00 +0900

    Camera Page Generation

--- osbprjt/src/pages/home/home.ts
+++ osbprjt/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { CameraPage } from '../camera/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +10,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  openCamera(): void{
+    this.navCtrl.push(CameraPage);
+  }
 }
