576564fa98b05b244b9184fe4048683ae41c6a28
yejin <mjin1102@dongguk.edu>
2019-05-07 18:16:52 +0900

    iframe

--- osbprjt/src/app/app.module.ts
+++ osbprjt/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
--- osbprjt/src/pages/home/home.ts
+++ osbprjt/src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform,AlertController } from 'ionic-angular';
+import { AnonymousSubject } from 'rxjs';
+
+import {HTTP} from '@ionic-native/http';
+//import {first} from '../next/next';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage  {
+  
+  
   constructor(public navCtrl: NavController) {
-
   }
+ 
+ 
 
 }
