cca7d1de5aa4c875bcfa07999c67d7a616db50d0
yejin <mjin1102@dongguk.edu>
2019-05-07 18:39:56 +0900

    modify conflicts

--- osbprjt/src/pages/home/home.ts
+++ osbprjt/src/pages/home/home.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-<<<<<<< HEAD
 import { Platform,AlertController } from 'ionic-angular';
 import { AnonymousSubject } from 'rxjs';
 
 import {HTTP} from '@ionic-native/http';
 //import {first} from '../next/next';
-
-
-=======
 import { CameraPage } from '../camera/camera';
->>>>>>> upstream/master
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -20,13 +15,7 @@ export class HomePage  {
   
   constructor(public navCtrl: NavController) {
   }
-<<<<<<< HEAD
- 
- 
-
-=======
   openCamera(): void{
     this.navCtrl.push(CameraPage);
   }
->>>>>>> upstream/master
 }
