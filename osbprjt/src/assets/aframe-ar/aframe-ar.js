a53c1040139e00d65157a7c0d0c5afc19c59c874
unknown <ui-jeong>
2019-06-07 03:52:46 +0900

    save

--- osbprjt/src/assets/aframe-ar/aframe-ar.js
+++ osbprjt/src/assets/aframe-ar/aframe-ar.js
@@ -1,7 +1,7 @@
 window.onload=function(){
   document.querySelector('a-text').addEventListener('click', function (evt){
       console.log('I was clicked at: ', evt.detail.intersection.point);
-      this.setAttribute('color','red');
+      this.setAttribute('color','black');
   });
   
   }
